b2dc80fc7b36adbe4090c054f36d325c28f3db51
IbrahimHassan502 <ih35840@gmail.com>
2022-05-06 03:29:10 +0200

    "update the 6th of 5/2022"

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-let zero = 0;
-
-let counter = 3;
-
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
-
-// Write Code Here
-for (let i = 0; i < 2; i++) {
-  my.pop();
+function multiply(...nums) {
+  let resault = 1;
+  for (let i = 0; i < nums.length; i++) {
+    resault *= typeof nums[i] === "number" ? parseInt(nums[i]) : 1;
+  }
+  return resault;
 }
-console.log(my.reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
 
-console.log(my.slice(++zero, counter)); // ["Elham", "Mazero"]
-
-console.log(
-  `${my[zero].slice(--zero, --counter)}${my[counter].slice(counter)}`
-); // "Elzero"
-console.log(
-  `${my[counter++][++counter]}${my[++zero + true][++counter].toUpperCase()}`
-); // "rO"
-console.log(zero, counter);
+console.log(multiply(100.5, 10, "B"));
