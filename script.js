e4608d6a03475f42378677428daf054e4c358b4d
IbrahimHassan502 <ih35840@gmail.com>
2022-07-12 23:34:54 +0200

    updated 12th of 7/2022 (problem solving & lessons 86-93

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let o = { p: 1, p: 2 };
-
-console.log(o.p);
-console.log(o);
+"use strict";
+let myInput = document.forms[0][0];
+let res = document.querySelector(".result");
+function changeCurrency() {
+  res.innerText = `{${myInput.value} USD Dollar = {${(
+    myInput.value * 5.16
+  ).toFixed(2)}} Egyptian Pound`;
+}
+myInput.onchange = changeCurrency();
