5691d5ae49069add5194c7a45a5f37bc49792300
IbrahimHassan502 <ih35840@gmail.com>
2022-07-21 20:54:35 +0200

    updated 21th of 7/2022

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-let divs = document.querySelectorAll(".test div");
-divs[0].title = divs[1].title;
-divs[0].innerText = divs[1].innerText;
-console.log(divs);
+function myFun(name) {
+  console.log(`hi ${name}`);
+}
+setTimeout(myFun("osama"), 5000);
